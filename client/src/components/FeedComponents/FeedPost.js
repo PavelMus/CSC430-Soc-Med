ff43054f8ab3d851f4581b3456821f1cf5ce25b7
Aaron <iaaron94@gmail.com>
2018-03-31 00:12:39 -0400

    changednewsfeed

--- client/src/components/FeedComponents/FeedPost.js
+++ client/src/components/FeedComponents/FeedPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import tempimg from '../../img/temp-user-img.jpg';
 class FeedPost extends Component {
   constructor(props){
     super(props);
@@ -9,34 +9,23 @@ class FeedPost extends Component {
   render() {
     return (
       <div>
-        <div id="post" className="card small">
-
-          <div className="card-top">
-            <img src="https://pbs.twimg.com/profile_images/952537094085271553/Le35qXfX.jpg" alt="" className="userimage responsive-img circle"/>
-            <div className="username">Milkman272</div>
-          </div>
-
-          <div className="post-content">
-            <p className="post-text">Hella lumbersexual thundercats pour-over, heirloom kinfolk salvia migas. Activated charcoal photo booth hella, schlitz helvetica quinoa neutra chicharrones prism slow-carb. Intelligentsia unicorn disrupt chillwave waistcoat</p>
-          </div>
-
-          <div className="activity-section">
-            <div className="user-actions">
-              <i className="fa fa-thumbs-o-up" aria-hidden="true"></i>
-              <i className="fa fa-commenting-o" aria-hidden="true"></i>
+        <div className="feed-post">
+          <h3><a href="">Nominations are Now Open - Student Leadership positions.</a></h3>
+            <div className="divider"></div>
+            <div className="feed-item-body">
+              <p>If you are experiencing hunger, the College of Staten Island has a Food Pantry.  The Food Pantry is located in the Green Dolphin Lounge in Building 1C, the Campus Center. The CSI Food Pantry will offer healthy food selections to currently enrolled CSI students.  All information concerning applications to this food program will be kept confidential.</p>
+              <a className="read-more-link" href="">Read more...</a>
             </div>
-            <div className="comments">
-              <ul>
-              <li><a title="username">BlackMilk111 </a><span>Nice post bro!</span></li>
-              <li><a title="username">JessicaRabbit72 </a><span>I appreciate this post.</span></li>
-              <li><a title="username">Killmonger96 </a><span>Honestly your amazing!</span></li>
-              </ul>
-            </div>
-            <div className="input-comment">
-              <input type="text" name="comment-input" placeholder="Add a comment here"/>
-            </div>
-          </div>
 
+            <div className="feed-item-footer">
+              <div className="feed-item-footer-left"><time class=" js-relative-time" datetime="2017-12-13T14:19:48Z" title="December 13, 2017  9:19 AM">March 30, 2018 at 9:19am</time> by
+              <div className="chip">
+                <img src={tempimg} alt="Contact Person"/>
+                Louis Petingi
+              </div>
+
+              </div>
+              </div>
         </div>
       </div>
     );
