78eefc7f3c8a445af07ad361f02603796b864e2e
Aaron <iaaron94@gmail.com>
2018-04-06 01:04:32 -0400

    fixed posting sections

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,22 +7,24 @@ class Landing extends Component {
   render() {
     return (
       <div id="content-section-container" className="container">
+
         <div className="row" id="content-area-row">
-          <div className="left-section col s12 m12 l3 xl3">
+          <div className="left-section col s12 m3 l3 xl3">
             <Leftsection />
           </div>
-          <div className="middle-right-section col s12 m12 l8 xl8">
+          <div className="middle-right-section col s12 m6 l8 xl8">
           <div className="row" id="middle-right-row">
             <Newsfeed
               pollInterval = {5000}
 
             />
-            <div className="col s12 m3 l3 xl4">
+            <div className="col s12 m2 l4 xl4">
               <Rightsection />
             </div>
           </div>
           </div>
         </div>
+
       </div>
     );
   }
