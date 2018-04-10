8b798de64039c5fc7addfd305caf5d700077067e
Pavel Musatov <pavel.musatov@cix.csi.cuny.edu>
2018-04-10 00:04:17 -0400

    light refactoring

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -8,21 +8,11 @@ class FrontPage extends Component {
     return (
       <div id="content-section-container" className="container">
         <div className="row" id="content-area-row">
-
           <div className="col s12 m2 l3 xl3">
             <Fixedmenu />
           </div>
-
-          <div className="middle-right-section col s12 m6 l9 xl9">
-            <div className="row" id="middle-right-row">
-              <Newsfeed pollInterval = {5000}/>
-              <div className="left-section col s12 m3 l4 xl4">
-                <AlertSection />
-              </div>
-            </div>
-          </div>
-
-
+          <Newsfeed pollInterval = {5000}/>
+          <AlertSection />
         </div>
       </div>
     );
