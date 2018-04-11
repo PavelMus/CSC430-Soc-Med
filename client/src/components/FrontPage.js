e9e26f170ac66cf3f91ed15c302b25d172cffeb4
Pavel Musatov <pavel.musatov@cix.csi.cuny.edu>
2018-04-11 14:30:59 -0400

    merge fix

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -11,20 +11,8 @@ class FrontPage extends Component {
           <div className="col s12 m2 l3 xl3">
             <Fixedmenu />
           </div>
-<<<<<<< HEAD
-
-          <div className="middle-right-section col s12 m6 l9 xl9">
-            <div className="row" id="middle-right-row">
-              <Newsfeed pollInterval = {5000}/>
-              <div className="left-section col s12 m3 l4 xl4">
-                <AlertSection />
-              </div>
-            </div>
-          </div>
-=======
           <Newsfeed pollInterval = {5000}/>
           <AlertSection />
->>>>>>> 8b798de64039c5fc7addfd305caf5d700077067e
         </div>
       </div>
     );
